b874e6228043bc6c13b24b2c39726da4e204b732
mdk-klm <antoine.04@laposte.net>
2021-02-04 09:18:41 +0100

    first commit

---
+++ src/main/java/com/sskorupski/learn/springboot/users/UserDAO.java
@@ -0,0 +1,15 @@
+package com.sskorupski.learn.springboot.users;
+
+import java.util.List;
+
+public interface UserDAO {
+
+    List<User> getAll();
+    User getById(Long id);
+
+    boolean delete(User user);
+
+    boolean update(User user);
+
+    Long create(User user);
+}
